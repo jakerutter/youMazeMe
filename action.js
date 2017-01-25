77ac73fab4ce858a1dae29b76513102b0139848c
jakerutter <rutter.jacob@gmail.com>
2017-01-25 15:30:11 -0500

    Random numbers Goal(X,Y) generated.
    Currently X and Y are random from 0,14 (matching the number of rows and columns) but I will be changing X to allow for only 0 or 14 so that the Goal (Maze Exit) is always on the perimeter of the grid.

--- action.js
+++ action.js
@@ -7,12 +7,17 @@ function (){
     $('#htmlloc').html("");
     });
 
-   
- $('.GoalPositionX').hover(function () {
-  var xGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;    
+var xGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+var yGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+ $('.GoalPositionX').hover(function () {    
 ($('#xGoalPos').html (xGoalPos))},
 function (){
-    $('#xGoalPos').html("");
-    });   
+    $('#xGoalPos').html(xGoalPos);
+    });
+ $('.GoalPositionY').hover(function () {    
+($('#yGoalPos').html (yGoalPos))},
+function (){
+    $('#yGoalPos').html(yGoalPos);
+    });      
 
     

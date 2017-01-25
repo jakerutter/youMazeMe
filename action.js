f69e03b6ac1e620911accd3e88f8a0c1cbd8c73f
jakerutter <rutter.jacob@gmail.com>
2017-01-25 15:07:27 -0500

    Added 2 Divs
    Added 2 Divs to hold random numbers generated to determine the starting "Goal" (Exit of Maze). Currently using the hover function to view the random number, but needs improving as the random number is changing per hover and that is not desired.

--- action.js
+++ action.js
@@ -1,11 +1,18 @@
-<script>
- $("#locationdisp").hover(
-function ()
-{
-    $(this).html($("<span> HOVERING!!!!! </span>"));
-},
-function ()
-{
-$(this).html($(""))};
-});
-</script>
+
+
+
+$('.mazehole').hover(function () {
+($('#htmlloc').html (this.id))},
+function (){
+    $('#htmlloc').html("");
+    });
+
+   
+ $('.GoalPositionX').hover(function () {
+  var xGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;    
+($('#xGoalPos').html (xGoalPos))},
+function (){
+    $('#xGoalPos').html("");
+    });   
+
+    

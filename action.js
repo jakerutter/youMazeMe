6ffd9134e02f63e0c12b0bd1884816641dd773fb
jakerutter <rutter.jacob@gmail.com>
2017-01-25 19:23:58 -0500

    CSS updates

--- action.js
+++ action.js
@@ -1,23 +1,31 @@
 
-
-
 $('.mazehole').hover(function () {
 ($('#htmlloc').html (this.id))},
 function (){
     $('#htmlloc').html("");
     });
-
+var xint = 0;
+var yint = 0;
 var xGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
 var yGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
- $('.GoalPositionX').hover(function () {    
+ $('.GoalPositionX').hover(function () {
+     return xGoalPos;    
 ($('#xGoalPos').html (xGoalPos))},
-function (){
+function (xint){
     $('#xGoalPos').html(xGoalPos);
     });
- $('.GoalPositionY').hover(function () {    
+ $('.GoalPositionY').hover(function () {
+     if (yGoalPos >= 6){
+         yGoalPos = 14
+         return yGoalPos;
+     } 
+     else {
+     (yGoalPos = 0)
+     return yGoalPos;
+     ;}
 ($('#yGoalPos').html (yGoalPos))},
-function (){
+function (yint){
     $('#yGoalPos').html(yGoalPos);
     });      
 
-    
+

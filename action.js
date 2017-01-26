ac2b9883296b4017b3ed90248c23cc1ace64e238
jakerutter <rutter.jacob@gmail.com>
2017-01-26 18:21:01 -0500

    X,Y coords both generated when hover over 1 area

--- action.js
+++ action.js
@@ -1,20 +1,22 @@
-
+//Colors the MAZEHOLE gray when it is hovered over.
 $('.mazehole').hover(function () {
 ($('#htmlloc').html (this.id))},
 function (){
     $('#htmlloc').html("");
     });
+
+//Selects 2 Random numbers to create an EXIT for the maze on the perimeter.
 var xint = 0;
 var yint = 0;
 var xGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
 var yGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
- $('.GoalPositionX').hover(function () {
+ $('.GoalPositionX').hover(function (xint) {
      return xGoalPos;    
 ($('#xGoalPos').html (xGoalPos))},
-function (xint){
+function (){
     $('#xGoalPos').html(xGoalPos);
     });
- $('.GoalPositionY').hover(function () {
+ $('.GoalPositionX').hover(function (yint) {
      if (yGoalPos >= 6){
          yGoalPos = 14
          return yGoalPos;
@@ -27,6 +29,14 @@ function (xint){
 function (yint){
     $('#yGoalPos').html(yGoalPos);
     });      
+//This will color the selected ID ^ a different color to mark it as the exit of the maze.
+
+//$('#').html(function(xGoalPos,yGoalPos){ $("id-" + xGoalPos + "," + yGoalPos).css("background-color", "#FBB");
+//});
+
+
+
+
 
 
 //This can generate divs programatically

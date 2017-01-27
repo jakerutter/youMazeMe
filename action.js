faab415ed9987837d8f034220a54de80b46109d5
jakerutter <rutter.jacob@gmail.com>
2017-01-27 00:47:35 -0500

    Entry and Exit squares updated
    Both entry and exit squares generate a position along perimeter across from one another and do so in a fashion that makes the maze either vertical or horizontal for that round. Entry colors itself blue, exit colors itself red. 
    Next to do is create randomly generated restraints (walls) and create algorithm for movement.

--- action.js
+++ action.js
@@ -5,29 +5,72 @@ function (){
     $('#htmlloc').html("");
     });
 
-//Selects 2 Random numbers to create and ENTRY & EXIT for the maze on the perimeter.
+//Create a random number to determine if the maze will orient and constrain the Entry and Exit
+//vertically or horizontally away from one another.
+var upAndDown = true;
 
-var xGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
-var yGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
-var xEntryPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
-var yEntryPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+var Orientation = Math.floor(Math.random()*2);
+if (Orientation >= 1)
+    { upAndDown = true;}
+else
+    { upAndDown = false;}
+//Selects 2 Random numbers to create and ENTRY & EXIT for the maze on the perimeter.
+if (upAndDown)
+    {
+    var xGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+    var yGoalPos = Math.floor(Math.random());
+        if (yGoalPos >=.5)
+            {yGoalPos = 14;}
+        else 
+            {yGoalPos = 0;}
+    var xEntryPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+    var yEntryPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+        if (yGoalPos == 14)
+            {yEntryPos = 0;}
+        else
+            {yEntryPos = 14;}    
+    }
+else {
+    var xGoalPos = Math.floor(Math.random());
+        if (xGoalPos >=.5)
+            {xGoalPos = 14;}
+        else 
+            {xGoalPos = 0;}
+    var yGoalPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+    var yEntryPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+    var xEntryPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+        if (xGoalPos == 14)
+            {xEntryPos = 0;}
+        else
+            {xEntryPos = 14;}    
+}
  $('.GoalPositionX').text(xGoalPos)
  $('.GoalPositionY').text(yGoalPos)
  $('.EntryPositionX').text(xEntryPos)
  $('.EntryPositionY').text(yEntryPos)
      
- //selects the random number and gets them into html id format for comparisons.
+ //selects the random number and gets them into html grid-id format for comparisons.
 var entryNum = (xEntryPos+","+yEntryPos);
 var exitNum = (xGoalPos+","+yGoalPos);
-var entryID = ("#"+entryNum);
-var exitID = ("#"+exitNum);
 
+//Colors the Entry square
 $(".mazehole").each(function() {
    var isEntry = $(this).attr('id');
-   if( isEntry == entryID || isEntry == entryNum)
+   if( isEntry == entryNum)
       document.getElementById(isEntry).style.backgroundColor = "lightblue";
 
 });
+//Colors the Exit square
+$(".mazehole").each(function() {
+   var isExit = $(this).attr('id');
+   if(isExit == exitNum)
+      document.getElementById(isExit).style.backgroundColor = "red";
+
+});
+
+
+
+
 
 
 //This can generate divs programatically

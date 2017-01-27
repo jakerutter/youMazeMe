705f108bd0c4fa0f059d8ff0e47a5cba5911a34e
jakerutter <rutter.jacob@gmail.com>
2017-01-26 22:17:57 -0500

    Added random entry point numbers
    CSS update, js update to include entry numbers

--- action.js
+++ action.js
@@ -23,16 +23,39 @@ function (){
      } 
      else {
      (yGoalPos = 0)
-     return yGoalPos;
-     ;}
+     return yGoalPos;}
 ($('#yGoalPos').html (yGoalPos))},
 function (yint){
     $('#yGoalPos').html(yGoalPos);
     });      
-//This will color the selected ID ^ a different color to mark it as the exit of the maze.
 
-//$('#').html(function(xGoalPos,yGoalPos){ $("id-" + xGoalPos + "," + yGoalPos).css("background-color", "#FBB");
-//});
+//Selects 2 Random numbers to create an Entrance for the maze on the perimeter.
+var xint = 0;
+var yint = 0;
+var xEntryPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+var yEntryPos = Math.floor(Math.random() * (14 - 0 + 1)) + 0;
+ $('.EntryPositionX').hover(function (xint) {
+     return xEntryPos;    
+($('#xEntryPos').html (xEntryPos))},
+function (){
+    $('#xEntryPos').html(xEntryPos);
+    });
+ $('.EntryPositionX').hover(function (yint) {
+     if (yEntryPos >= 6){
+         yEntryPos = 14
+         return yEntryPos;
+     } 
+     else {
+     (yEntryPos = 0)
+     return yEntryPos;}
+($('#yEntryPos').html (yEntryPos))},
+function (yint){
+    $('#yEntryPos').html(yEntryPos);
+    });      
+
+//This will color the selected ID ^ a different color to mark it as the entrance/exit of the maze.
+
+var markEntry = (function(xGoalPos,yGoalPos){ $("id-" + xGoalPos + "," + yGoalPos).css("background-color", "#FBB");});
 
 
 
